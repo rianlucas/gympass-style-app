151979f8214a157520f2096eb2786b8ddc86fb22
Rian Gomes <rianluccas08@gmail.com>
2024-07-25 20:31:34 -0300

    feat: create gyms repository

---
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -0,0 +1,10 @@
+import { Gym } from '@prisma/client'
+import { GymsRepository } from '../gyms-repository'
+
+export class InMemoryGymsRepository implements GymsRepository {
+  public items: Gym[] = []
+
+  async findById(id: string) {
+    return this.items.find((item) => item.id === id) || null
+  }
+}
